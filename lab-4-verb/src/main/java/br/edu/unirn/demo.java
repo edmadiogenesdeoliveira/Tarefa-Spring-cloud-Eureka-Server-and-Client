b9033ec568f1c6e1a4292bf699acea2b2e7c94e8
edmar <edmadiogenesdeoliveira@hotmail.com>
2016-09-21 17:06:36 -0300

    Tarefa pra fazer: MicroService Spring Cloud Eureka Server and Client

---
+++ lab-4-verb/src/main/java/br/edu/unirn/demo.java
@@ -0,0 +1,29 @@
+package br.edu.unirn;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class demo {
+
+	@Value("${words}")
+	private String words;
+	
+	public String getWords() {
+		return words;
+	}
+
+	public void setWords(String words) {
+		this.words = words;
+	}
+
+	@RequestMapping("/")
+	public @ResponseBody String getWord() {
+		String[] wordArray = words.split(",");
+		int i = (int)Math.round(Math.random() * (wordArray.length - 1));
+		return wordArray[i];
+	}
+	
+}
